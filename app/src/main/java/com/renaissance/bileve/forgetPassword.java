6f9880ccb429a780779dd61785e491818ceb6671
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-11-08 13:02:12 +0300

    Updates: -created box.xml for design -forget password ui, home ui, login page ui

---
+++ app/src/main/java/com/renaissance/bileve/forgetPassword.java
@@ -0,0 +1,14 @@
+package com.renaissance.bileve;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class forgetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget_password);
+    }
+}
---
+++ app/src/main/java/com/renaissance/bileve/homePage.java
@@ -0,0 +1,14 @@
+package com.renaissance.bileve;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class homePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
