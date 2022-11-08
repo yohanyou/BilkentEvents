3a444c9e91a5388cd260c99d9f28fcc457e1e357
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-11-08 14:28:51 +0300

    Updates: -sorted the strings.xml file -created settings.xml file

---
+++ app/src/main/java/com/renaissance/bileve/settings.java
@@ -0,0 +1,14 @@
+package com.renaissance.bileve;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
