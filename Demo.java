4e4b2bed2081a2f3711b6781b5dbe42f9fcb883d
Ali <majidiali165@gmail.com>
2022-11-24 20:01:40 +0300

    Test

--- Demo.java
+++ Demo.java
@@ -9,17 +9,22 @@ import java.sql.ResultSet;
 public class Demo {
 
     public static void main(String[] args) {
+        // Your Database address
         String url ="jdbc:mysql://localhost:3306/jdbc";
+        // Do not change it
         String username="root";
+        // Deafault password
         String password="";
         try {
+        // Do not change it except the jdbc part
         Class.forName("com.mysql.cj.jdbc.Driver");
-
+        // Do not change it
         Connection connection = DriverManager.getConnection(url,username,password);
-
+        // Do not change it
         Statement statement = (Statement) connection.createStatement();
         ResultSet resultSet = statement.executeQuery();
         } catch (Exception e) {
+            // Do not change it
             System.out.println(e);
         }
     }
---
+++ test.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+public class test {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+    }
+}
