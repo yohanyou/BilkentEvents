17d874553e1722d0df445356b22fdaf9ef713084
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-12-12 14:43:56 +0300

    Updates: -Added activity intents for changing between xml/java files -Login button in the login.xml now pairs with loginPage.java and sends the user to home_page.xml -changed some strings.xml information

--- app/src/main/java/com/renaissance/bileve/loginPage.java
+++ app/src/main/java/com/renaissance/bileve/loginPage.java
@@ -2,7 +2,10 @@ package com.renaissance.bileve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginPage extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class loginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        //creates a button and then attaches it to the login button
+        Button button = (Button) findViewById(R.id.login);
+        //sets an on on click listener to this button
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //whenever the button is clicked it executes the homePage method
+                homePage();
+            }
+
+        });
     }
+
+    public void homePage(){
+        //creates a new intent
+        Intent intent = new Intent(this, homePage.class);
+        //changes from this Activity to the homePage class
+        startActivity(intent);
+    }
+
 }
