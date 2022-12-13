64f20495814176f9d574e8318c8d332c7092fa1e
Ali <majidiali165@gmail.com>
2022-12-13 12:19:58 +0300

    Add create account action listener in login page to go exactly to the create account

--- app/src/main/java/com/renaissance/bileve/loginPage.java
+++ app/src/main/java/com/renaissance/bileve/loginPage.java
@@ -9,12 +9,14 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class loginPage extends AppCompatActivity {
     EditText email, password;
     Button login;
     DBHelper DB;
+    TextView createaccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +25,15 @@ public class loginPage extends AppCompatActivity {
         email =findViewById(R.id.email1);
         password= findViewById(R.id.password1);
         login = findViewById(R.id.login);
+        createaccount =findViewById(R.id.create_account);
         DB= new DBHelper(this);
-
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),createAccount.class);
+                startActivity(intent);
+            }
+        });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
