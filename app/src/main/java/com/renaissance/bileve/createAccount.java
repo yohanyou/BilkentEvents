59edff5f9c5b37ee86e90381186d65bf19812491
Ali <majidiali165@gmail.com>
2022-12-13 10:14:23 +0300

    Database Crashed, so working on that way may crash program any time, I will try another way

--- app/src/main/java/com/renaissance/bileve/createAccount.java
+++ app/src/main/java/com/renaissance/bileve/createAccount.java
@@ -2,65 +2,15 @@ package com.renaissance.bileve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Looper;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputEditText;
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class createAccount extends AppCompatActivity {
-    TextInputEditText textInputEditTextEmail, textInputEditTextPassword, textInputEditTextID;
-    Button buttonlogin, buttoncreate;
-    TextView welcome;
-    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
-        textInputEditTextEmail =findViewById(R.id.email);
-        textInputEditTextID =findViewById(R.id.bilkentID);
-        textInputEditTextPassword =findViewById(R.id.password);
-        buttonlogin = findViewById(R.id.buttonlogin);
-        buttoncreate = findViewById(R.id.buttoncreate);
-        welcome = findViewById(R.id.welcome);
-        buttoncreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Start ProgressBar first (Set visibility VISIBLE)
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        //Starting Write and Read data with URL
-                        //Creating array for parameters
-                        String[] field = new String[2];
-                        field[0] = "id";
-                        field[1] = "email";
-                        //Creating array for data
-                        String[] data = new String[2];
-                        data[0] = "data-1";
-                        data[1] = "data-2";
-                        PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
-                        if (putData.startPut()) {
-                            if (putData.onComplete()) {
-                                String result = putData.getResult();
-                                //End ProgressBar (Set visibility to GONE)
-                            }
-                        }
-                        //End Write and Read data with URL
-                    }
-                });
-            }
-        });
-
-
-
-
 
     }
 }
