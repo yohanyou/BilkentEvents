84ac043cedfe0d84ffd6b0d31790c8ccafb89172
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-12-15 21:29:01 +0300

    Update: -fixed the AndroidManifest.xml file so it starts with the login.xml -made changes to the xml and java files so the button/textview presses actually take you to places
    TODO (self):
    -work on connecting the Events into either the homescreen or event_organizer.xml
    -work on other design aspects and create a clearer user documentation for specific button/textview ID's so we can code more efficiently

--- app/src/main/java/com/renaissance/bileve/homePage.java
+++ app/src/main/java/com/renaissance/bileve/homePage.java
@@ -3,16 +3,42 @@ package com.renaissance.bileve;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class homePage extends AppCompatActivity {
     TextView logout1;
+    ImageButton settings, eventApply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+
+
+        //creates a button-listener for the settings button
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), settings.class);
+                startActivity(intent);
+            }
+        });
+
+        //creates a button-listener for the event applying button
+        eventApply = findViewById(R.id.eventApply);
+        eventApply.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), eventOrganizer.class);
+                startActivity(intent);
+            }
+        });
+
+        //creates a button-listener for the logout button
         logout1 = findViewById(R.id.logout);
         logout1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,4 +48,5 @@ public class homePage extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/renaissance/bileve/settings.java
+++ app/src/main/java/com/renaissance/bileve/settings.java
@@ -1,15 +1,51 @@
 package com.renaissance.bileve;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class settings extends AppCompatActivity {
-    loginPage logp = new loginPage();
+    Button logout;
+    ImageButton homeButton;
+    TextView changePassword;
+
+    //unnecessary code but i have no idea who wrote this so it'll stay
+    //    loginPage logp = new loginPage();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),homePage.class);
+                startActivity(intent);
+            }
+        });
+
+        changePassword = findViewById(R.id.settingsChangePassword);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),changePassword.class);
+                startActivity(intent);
+            }
+        });
+
+            logout = findViewById(R.id.logoutButton);
+            logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(),loginPage.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
