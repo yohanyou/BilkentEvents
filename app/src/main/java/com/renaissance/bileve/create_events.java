e4aa10db77baab9b79c57fef9432ecce5f89be9d
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-12-20 21:27:41 +0300

    Update: -wip filterlist, working with kenan on this topic (commented)

--- app/src/main/java/com/renaissance/bileve/create_events.java
+++ app/src/main/java/com/renaissance/bileve/create_events.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class create_events extends AppCompatActivity {
 
@@ -22,9 +23,12 @@ public class create_events extends AppCompatActivity {
 
     MyDatabaseHelper myDB;
     ArrayList<String> event_id, event_title, event_lang, event_date, event_loc;
+    //ArrayList<String>filteredId, filteredTitle, filteredLang, filteredDate, filteredLoc;
+    //int indexNo, increment;
+    //customadapter customFilteredAdapter
     CustomAdapter customAdapter;
     SearchView searchView;
-    
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +48,7 @@ public class create_events extends AppCompatActivity {
                 return true;
             }
         });
+
         recyclerView =findViewById(R.id.recyclerView);
         add_button = findViewById(R.id.floatingActionButton);
         add_button.setOnClickListener(new View.OnClickListener() {
@@ -70,7 +75,29 @@ public class create_events extends AppCompatActivity {
     }
 
     private void filterList(String text) {
+        /*
+        filteredId = new ArrayList<>();
+        filteredTitle = new ArrayList<>();
+        filteredLang = new ArrayList<>();
+        filteredDate = new ArrayList<>();
+        filteredLoc = new ArrayList<>();
+        increment = 0;
 
+        for (String filter : event_date) {
+            if (filter.equals(text)) {
+                indexNo = event_date.indexOf(filter);
+                filteredId.set(increment, event_id.get(indexNo));
+                filteredTitle.set(increment, event_title.get(indexNo));
+                filteredLang.set(increment, event_lang.get(indexNo));
+                filteredLoc.set(increment, event_loc.get(indexNo));
+                increment++;
+            }
+        }
+
+        customFilteredAdapter = new CustomAdapter(create_events.this, filteredId, filteredTitle, filteredLang, filteredDate, filteredLoc);
+        recyclerView.setAdapter(customFilteredAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(create_events.this));
+         */
     }
 
     void storeDataInArrays(){
@@ -87,5 +114,4 @@ public class create_events extends AppCompatActivity {
             }
         }
     }
-
 }
