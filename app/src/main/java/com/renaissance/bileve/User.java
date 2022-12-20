eb93b3e4480d891554332e0fc34df39f992deda4
Maryam Azimli <maryam.azimli@ug.bilkent.edu.tr>
2022-12-20 14:45:52 +0300

    add events xml

---
+++ app/src/main/java/com/renaissance/bileve/User.java
@@ -0,0 +1,15 @@
+package com.renaissance.bileve;
+
+public class User {
+
+    public static void main(String[] args) {
+
+        Events e1 = new Events("a","b","c","d","f",50);
+        Events e2 = new Events("a","b","c","d","f",40);
+
+        Events e3 = new Events("a","b","c","d","f",60);
+        Events e4 = new Events("a","b","c","d","f",88);
+
+        Events e5 = new Events("a","b","c","d","f",770);
+    }
+}
