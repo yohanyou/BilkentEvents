f5ddc9b1487bc62ee0e0e3fd5b40a51729b64810
Ali <majidiali165@gmail.com>
2022-12-24 23:20:59 +0300

    Done 2

--- app/src/main/java/com/renaissance/bileve/createAccount.java
+++ app/src/main/java/com/renaissance/bileve/createAccount.java
@@ -6,17 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class createAccount extends AppCompatActivity {
+    public static int ithy;
     EditText email,password;
     Button login;
     DBHelper DB;
@@ -51,8 +50,6 @@ public class createAccount extends AppCompatActivity {
                 String em = email.getText().toString();
                 String pass = password.getText().toString();
 
-                // database helper methods called to check id and password
-
                 if (TextUtils.isEmpty(em) || TextUtils.isEmpty(pass))
                     Toast.makeText(createAccount.this,"All fields Required", Toast.LENGTH_SHORT).show();
                 else   {
@@ -60,7 +57,8 @@ public class createAccount extends AppCompatActivity {
                     if (checkuser==false){
                         Boolean insert = DB.insertData(pass,em);
                         if (insert==true){
-                            Toast.makeText(createAccount.this, "Registered Successfully",Toast.LENGTH_SHORT).show();
+                            ithy = (int) ((Math.random()*10.8)+1)*180;
+                            Toast.makeText(createAccount.this, "Registered Successfully, Your Security Code is: " + ithy,Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(getApplicationContext(),loginPage.class);
                             startActivity(intent);
                         }else{
